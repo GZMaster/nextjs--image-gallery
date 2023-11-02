a59f7f2a74e987e9e10ff3e22f4a8e54bb6c90bc
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-11-02 04:00:47 +0100

    changes home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
+import { Alert } from "@/components/bootstrap";
+
 export default function Home() {
 	return (
 		<div>
-			<h1>Home Page</h1>
+			<Alert variant="primary">
+				<p>
+					<strong>NextJS Image Gallery</strong> is a demo project to showcase
+					different NextJS features.
+				</p>
+
+				<ul>
+					<li>static generation</li>
+					<li>server-side rendering</li>
+					<li>incremental static regeneration</li>
+					<li>client-side rendering</li>
+					<li>meta-data API</li>
+					<li>routing</li>
+					<li>navigation</li>
+					<li>bootstrap</li>
+					<li>css modules</li>
+					<li>typescript</li>
+					<li>and more</li>
+				</ul>
+
+				<p className="mb-8">
+					Every page uses a different combination of these features. You can
+					inspect the source code to see how it works.
+				</p>
+			</Alert>
 		</div>
 	);
 }
